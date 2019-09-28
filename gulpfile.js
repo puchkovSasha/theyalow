b0bf659b6065cc1195ed2e4ef6eef4495753a098
PuchkovSasha <Itworkpuchkov@yandex.ru>
2019-09-28 12:24:45 +0300

    fix:  change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    sass= require('gulp-sass');
+    sass= require('gulp-sass'),
+    browserSync = require('browser-sync');
 var autoprefixer = require('gulp-autoprefixer');
 var concatCss = require('gulp-concat-css');
  
@@ -11,16 +12,28 @@ gulp.task('sass', function(){
             cascade: false
         }))
     .pipe(concatCss('main.css'))
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({stream: true}))
 });
-
+gulp.task('html', function(){
+    return gulp.src('src/*.html')
+    .pipe(browserSync.reload({stream: true}))
+});
 gulp.task('js', function(){
-    return gulp.src('src/js/*.js');
-    
+    return gulp.src('src/js/*.js')
+    .pipe(browserSync.reload({stream: true}))
 });
-
+gulp.task('browser-sync', function(){
+	   browserSync( {
+	   	server: {
+	   		baseDir: 'src'
+	   	},
+	   	notify: false
+	   });
+});
 gulp.task('watch', function(){
 	 gulp.watch('src/sass/*.sass', gulp.parallel('sass'));
-     gulp.watch('src/js/*.js', gulp.parallel('js'));
+	 gulp.watch('src/*.html', gulp.parallel('html'));// Наблюдение з
+     gulp.watch('src/js/*.js', gulp.parallel('js'));// Наблюдение з
 });
-gulp.task('default', gulp.parallel('sass','watch'));
+gulp.task('default', gulp.parallel('sass', 'browser-sync','watch'));
